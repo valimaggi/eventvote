9a23564e05e9891b804fb642f679c24f59da4ead
valimaggi <valimaggi@gmail.com>
2017-05-06 01:36:43 +0300

    winston logger added to project

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const morgan = require('morgan');
 const database = require('./config/database');
 const applyMiddleware = require('./common/middleware').applyMiddleware;
 const helmetFunctions = require('./config/security');
+const winston = require('winston');
 require('dotenv').config();
 const API_PREFIX = require('./config/properties').API_PREFIX;
 const createRootRouter = require('./root-router');
@@ -18,14 +19,13 @@ database.connect(process.env.DB_URI, process.env.DB_USER, process.env.DB_PASS)
 .then(() => {
   // Set the root router with the API prefix
   app.use(API_PREFIX, createRootRouter());
-
   // Start the server
   const port = process.env.SERVER_PORT;
-  if (!port) throw new Error('No port');
+  if (!port) throw new Error('No port given');
   app.listen(port);
-  console.log(`Listening on port ${port}`);
+  winston.info(`Listening on port ${port}`);
 })
 .catch((error) => {
-  console.log(`Connection error: ${error}`);
+  winston.error(error);
   process.exit(0);
 });
